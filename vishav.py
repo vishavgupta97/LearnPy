14025643fb622ecb445ffe74c4493e95dec837af
vishav <vishavgupta988@gmail.com>
2019-10-23 13:09:28 +0530

    Vishav Modified

--- vishav.py
+++ vishav.py
@@ -1,7 +1,36 @@
-a = 5
-b = 6
-a = (a+b)-a
-b = (a+b)-a
-print("%d and %d" % (a, b))
-print("%d is %d years old." % (a, b))
-a = a+b
+import sys
+import demo
+
+
+def fact(n):
+    if n == 1:
+        return n
+    else:
+        return n * fact(n - 1)
+
+
+a = (int(input("Enter Number\n")))
+i = 2
+x = 0
+if a <= 1:
+    print("not prime")
+    sys.exit()
+while i <= int(a / 2):
+    if a % i == 0:
+        print("NOT PRIME")
+        print(i)
+        x = 1
+        break
+    i = i + 1
+if x == 0:
+    print("PRIME")
+d = (int(input("Enter Factorial Number\n")))
+c = fact(d)
+print(c)
+for x in demo.b:
+    print(demo.b)
+
+f = [2, 3, a]
+a = {'v': 5, 'b': 2}
+print(a['v'])
+print(f)
